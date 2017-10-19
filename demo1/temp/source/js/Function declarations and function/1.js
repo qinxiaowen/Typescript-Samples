c35f212d485a202c4d80b634e93adaeed242abc8
qinxiaowen <453000697@qq.com>
2017-10-19 18:17:01 +0800

    update

---
+++ demo1/temp/source/js/Function declarations and function/1.js
@@ -0,0 +1,12 @@
+console.log(greetNamed("John"));
+console.log(greetUnnamed("John"));
+function greetNamed(name) {
+    if (name) {
+        return "Hi! " + name;
+    }
+}
+var greetUnnamed = function (name) {
+    if (name) {
+        return "Hi! " + name;
+    }
+};
